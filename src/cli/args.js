2c26f27e479f2073be80be23e5bd11a37a84453c
pylkonata <pylko.nata@gmail.com>
2025-11-16 00:42:03 +0200

    feat: add command line solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-  // Write your code here
+  const lineArg = process.argv.reduce((acc, name, index, arr) => {
+    if (name.startsWith("--") && index < arr.length - 1) {
+      const modifiedName = name.slice(2);
+      if (index === arr.length - 2) {
+        acc += `${modifiedName} is ${arr[index + 1]}`;
+      } else {
+        acc += `${modifiedName} is ${arr[index + 1]}, `;
+      }
+    }
+    return acc;
+  }, "");
+  console.log(lineArg);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-  // Write your code here
+  const allEnvs = process.env;
+  const rssEnvs = Object.entries(allEnvs)
+    .filter(([name]) => name.startsWith("RSS_"))
+    .reduce((acc, [name, value], index, arr) => {
+      if (index === arr.length - 1) {
+        acc += `${name}=${value}`;
+      } else {
+        acc += `${name}=${value}; `;
+      }
+      return acc;
+    }, "");
+  console.log(rssEnvs);
 };
 
 parseEnv();
